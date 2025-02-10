b1ba77a03a61b31b6a74e45138777834840301e5
PreethiKalyani99 <preethikalyani99@gmail.com>
2025-02-10 18:39:53 +0530

    add onChange function for autocomplete

--- src/components/common/form/Form.tsx
+++ src/components/common/form/Form.tsx
@@ -10,14 +10,16 @@ interface CustomFormProps {
     onSubmit: (e: React.FormEvent) => void
     isLoading?: boolean
     buttonText?: string
+    onOptionChange?: (selected: any) => void
 }
-export const CustomForm: React.FC<CustomFormProps> = ({ fields, onChange, onSubmit, isLoading = false, buttonText = 'Submit', onSearch }) => {
+export const CustomForm: React.FC<CustomFormProps> = ({ fields, onChange, onSubmit, isLoading = false, buttonText = 'Submit', onSearch, onOptionChange }) => {
     return (
         <Form onSubmit={onSubmit}>
             <FormFields
                 fields={fields}
                 onChange={onChange}
                 onSearch={onSearch}
+                onOptionChange={onOptionChange}
             />
             <Button
                 type="submit"
@@ -33,11 +35,13 @@ export const CustomForm: React.FC<CustomFormProps> = ({ fields, onChange, onSubm
 interface FormFieldsProps {
     fields: Field[]
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
-    onSearch?: (selected: any) => void
+    onSearch?: (str: string) => void
+    onOptionChange?: (selected: any) => void
 }
 
-const FormFields: React.FC<FormFieldsProps> = ({ fields, onChange, onSearch }) => {
+const FormFields: React.FC<FormFieldsProps> = ({ fields, onChange, onSearch, onOptionChange }) => {
     const handleSearch = onSearch || (() => {})
+    const handleOptionChange = onOptionChange || (() => {})
     return (
         <>
             {fields.map(field => (
@@ -46,7 +50,8 @@ const FormFields: React.FC<FormFieldsProps> = ({ fields, onChange, onSearch }) =
                     {field?.autocomplete ?
                         <Autocomplete
                             field={field}
-                            onChange={handleSearch}
+                            onChange={handleOptionChange}
+                            onInputChange={handleSearch}
                         />
                         :
                         <TextFields
@@ -82,15 +87,18 @@ const TextFields: React.FC<TextFieldsProps> = ({ field, onChange }) => {
 interface AutocompleteProps {
     field: Field
     onChange: (selected: any) => void
+    onInputChange: (str: string) => void
 }
 
-const Autocomplete: React.FC<AutocompleteProps> = ({ field, onChange }) => {
+const Autocomplete: React.FC<AutocompleteProps> = ({ field, onChange, onInputChange }) => {
     return (
         <Typeahead
             id={`autocomplete-${field.name}`}
-            onInputChange={onChange}
+            onInputChange={onInputChange}
             options={field.option || []}
             placeholder={field.placeholder}
+            selected={field.value ? [field.value] : []}
+            onChange={onChange}
         />
     )
 }
