43259e6be70d7b4541a9cb1c3a81d64dba8fea4a
PreethiKalyani99 <preethikalyani99@gmail.com>
2025-02-10 18:44:31 +0530

    add states in use book hook

--- src/hooks/useBook.tsx
+++ src/hooks/useBook.tsx
@@ -1,13 +1,27 @@
 import { useState, useMemo, useEffect, createContext, useContext } from "react"; 
 import { ProviderProp, BookForm } from "../types";
 
+interface FormProp {
+    authorName: string
+    title: string
+    publishedYear?: string
+}
+
 interface BookContextProps {
-    formData:  BookForm
-    setFormData: (formData: BookForm) => void
+    formData:  FormProp
+    setFormData: (formData: FormProp) => void
     isAlertVisible: boolean
     setIsAlertVisible: (visible: boolean) => void
     bookData: BookForm[]
     setBookData: (value: React.SetStateAction<BookForm[]>) => void
+    count: number
+    setCount: (count: number) => void
+    currentPage: number
+    setCurrentPage: (page: number) => void
+    rowsPerPage: number
+    setRowsPerPage: (page: number) => void
+    query: string
+    setQuery: (str: string) => void
 }
 
 export const BookContext = createContext<BookContextProps>({
@@ -17,12 +31,24 @@ export const BookContext = createContext<BookContextProps>({
     setIsAlertVisible: () => null,
     bookData: [],
     setBookData: () => {},
+    count: 0,
+    setCount: () => null,
+    currentPage: 1,
+    setCurrentPage: () => null,
+    rowsPerPage: 10,
+    setRowsPerPage: () => null,
+    query: '',
+    setQuery: () => null,
 })
 
 export const BookProvider = ({children}: ProviderProp) => {
     const [bookData, setBookData] = useState<BookForm[]>([])
     const [isAlertVisible, setIsAlertVisible] = useState(false)
-    const [formData, setFormData] = useState<BookForm>({
+    const [count, setCount] = useState(0)
+    const [currentPage, setCurrentPage] = useState(1)
+    const [rowsPerPage, setRowsPerPage] = useState(10)
+    const [query, setQuery] = useState('')
+    const [formData, setFormData] = useState<FormProp>({
        authorName: '', 
        title: '', 
        publishedYear: ''
@@ -43,11 +69,23 @@ export const BookProvider = ({children}: ProviderProp) => {
            setBookData,
            isAlertVisible,
            setIsAlertVisible,
+           count,
+           setCount,
+           currentPage,
+           setCurrentPage,
+           rowsPerPage,
+           setRowsPerPage,
+           query,
+           setQuery,
         }
     }, [
         formData,
         bookData,
         isAlertVisible,
+        count,
+        currentPage,
+        rowsPerPage, 
+        query
     ])
 
     return <BookContext.Provider value={value}>{children}</BookContext.Provider>
