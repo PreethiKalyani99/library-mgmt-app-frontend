1ff6f569377f891ef789056074c5990ef7331f58
PreethiKalyani99 <preethikalyani99@gmail.com>
2025-02-11 21:20:26 +0530

    add routes for user and borrower components

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Signup from './components/signup/Signup';
 import Main from './components/Main';
 import Authors from './components/authors/Authors';
 import Books from './components/books/Books';
+import Users from './components/users/Users';
+import Borrowers from './components/borrowed-books/Borrowers';
 import './App.css';
 
 function App() {
@@ -30,6 +32,24 @@ function App() {
             <PrivateRoute 
               component={<Books />} 
               allowedRoles={[roles.ADMIN, roles.LIBRARIAN, roles.READER, roles.RECEPTIONIST]}
+            />
+          } 
+        />  
+        <Route 
+          path='users' 
+          element={
+            <PrivateRoute 
+              component={<Users />} 
+              allowedRoles={[roles.ADMIN]}
+            />
+          } 
+        />  
+        <Route 
+          path='borrowed-books' 
+          element={
+            <PrivateRoute 
+              component={<Borrowers />} 
+              allowedRoles={[roles.ADMIN, roles.LIBRARIAN, roles.RECEPTIONIST, roles.READER]}
             />
           } 
         />  
